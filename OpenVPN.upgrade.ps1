056ec362d8602b3b6647484acfd153c1a3daefb8
GethDeeo <21061348+GethDeeo@users.noreply.github.com>
2022-10-10 08:06:33 +0200

    [OpenVPN] fixed quoting

--- OpenVPN.upgrade.ps1
+++ OpenVPN.upgrade.ps1
@@ -1,3 +1,3 @@
 & winget.exe upgrade --accept-package-agreements --accept-source-agreements --source winget --exact --id OpenVPNTechnologies.OpenVPN
 
-Remove-Item -LiteralPath $env:PUBLIC\Desktop\OpenVPN GUI.lnk, "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\OpenVPN\Shortcuts" -Recurse -ErrorAction SilentlyContinue
+Remove-Item -LiteralPath "$env:PUBLIC\Desktop\OpenVPN GUI.lnk", "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\OpenVPN\Shortcuts" -Recurse -ErrorAction SilentlyContinue
